8b254423c0bd508adf441920b97739b2d1583352
Matthew Davis <matthew@matthewdavis.io>
2020-02-26 18:17:20 -0500

    bump

--- includes/ng.mk
+++ includes/ng.mk
@@ -1,19 +1,23 @@
+## Build a library (make build PROJECT=foolibrary)
 build:
 
 	ng build --prod $(PROJECT)
 
+## Builds all libraries
 build/all:
 
 	@for F in $(PROJECTS); do ng build --prod $$F; done
 
+## Publishes a library to npm (make publish PROJECT=foolibrary)
 publish:
 
 	rm -rf dist/$(PROJECT)
-	cd projects/$(PROJECT) && git commit -am'bump' || true
+	cd projects/$(PROJECT) && git add . && git commit -am'bump' || true
 	cd projects/$(PROJECT) && npm version patch
 	ng build --prod $(PROJECT) && cd dist/$(PROJECT) && npm publish --access=public
 
+## Builds + watches for changes for a library (make watch PROJECT=foolibrary)
 watch:
 
-	rm -rf dist/$(PROJECT)
-	nodemon -e ts -w projects/$(PROJECT)/src -x 'make build PROJECT=$(PROJECT) && ng serve'
+	#rm -rf dist/$(PROJECT)
+	nodemon -e ts,scss -d 1 -w projects/$(PROJECT)/src -x 'make build PROJECT=$(PROJECT) && ng serve'
